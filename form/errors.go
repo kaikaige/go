c6ada8b8d6fb89dc78d756bc2430d4db39cd0efe
高凯 <kaikaige007@gmail.com>
2021-07-16 16:28:40 +0800

    err

--- form/errors.go
+++ form/errors.go
@@ -1,13 +1,80 @@
 package form
 
-type BadRequest string
+import (
+	"fmt"
+	"github.com/go-playground/validator/v10"
+	"net/http"
+	"reflect"
+	"strconv"
+)
 
-func (e BadRequest) Error() string { return string(e) }
+type HttpError struct {
+	Code int `json:"code"`
+	Message string `json:"message"`
+}
 
-type NotFound string
+func (e HttpError) Error() string { return e.Message }
 
-func (e NotFound) Error() string { return string(e) }
+func BadRequest (ms ...string) HttpError {
+	message := "Bad Request"
+	if len(ms) > 0 {
+		message = ms[0]
+	}
+	return HttpError{
+		Code: http.StatusBadRequest,
+		Message: message,
+	}
+}
 
-type FormValidate string
+func NotFound (ms ...string) HttpError {
+	message := "Not Found"
+	if len(ms) > 0 {
+		message = ms[0]
+	}
+	return HttpError{
+		Code: http.StatusNotFound,
+		Message: message,
+	}
+}
 
-func (e FormValidate) Error() string { return string(e) }
+func ValidateError (ms ...string) HttpError {
+	message := "Form Validate Error"
+	if len(ms) > 0 {
+		message = ms[0]
+	}
+	return HttpError{
+		Code: http.StatusUnprocessableEntity,
+		Message: message,
+	}
+}
+
+func GetError(err error, ref interface{}) (code int, errMsg string) {
+	code = 400
+	switch e := err.(type) {
+	case validator.ValidationErrors:
+		if ref, found := reflect.ValueOf(ref).Type().FieldByName(e[0].StructField()); found {
+			code = 422
+			if errMsg = ref.Tag.Get(e[0].ActualTag() + "-msg"); errMsg == "" {
+				var label string
+				if label = ref.Tag.Get("label"); label == "" {
+					label = e[0].StructField()
+				}
+				switch e[0].ActualTag() {
+				case "required":
+					errMsg = label + "不能为空"
+				default:
+					errMsg = label
+				}
+			}
+		}
+	case *strconv.NumError:
+		code = 422
+		errMsg = e.Num + "不是一个数字"
+	case HttpError:
+		code = e.Code
+		errMsg = e.Message
+	default:
+		errMsg = fmt.Sprintf("未知异常：%T, msg: %s\n", err, err.Error())
+	}
+	return
+}
--- go.mod
+++ go.mod
@@ -4,4 +4,5 @@ go 1.16
 
 require (
 	github.com/gin-gonic/gin v1.7.1
+	"github.com/go-playground/validator/v10" v10
 )
